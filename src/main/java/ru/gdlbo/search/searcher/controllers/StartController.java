56a04a1e881c4429aee1bc0ab740335ab1a2a084
gdlbo <41114720+gdlbo@users.noreply.github.com>
2024-05-14 15:17:35 +0300

    wip runtime change config

--- src/main/java/ru/gdlbo/search/searcher/controllers/StartController.java
+++ src/main/java/ru/gdlbo/search/searcher/controllers/StartController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.gdlbo.search.searcher.config.RestartManager;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,20 +20,25 @@ public class StartController {
     private RestartManager restartManager;
 
     @PostMapping("/api/submit")
-    public String submitForm(@RequestParam("inputText") String inputText) throws IOException {
-        // Save inputText to application.properties
+    public String submitForm(@RequestParam("inputText") String inputText, @RequestParam(required = false, defaultValue = "false") String isDebug) throws IOException {
         Path propertiesPath = Paths.get(System.getProperty("user.dir"), "application.properties");
         Properties properties = new Properties();
+
         if (Files.exists(propertiesPath)) {
-            properties.load(Files.newInputStream(propertiesPath));
+            try (FileInputStream inStream = new FileInputStream(propertiesPath.toFile())) {
+                properties.load(inStream);
+            }
         }
+
         properties.setProperty("searcher.path", inputText);
-        properties.setProperty("searcher.isDebug", "false");
-        properties.store(new FileOutputStream(propertiesPath.toFile()), null);
+        properties.setProperty("searcher.isDebug", isDebug);
+
+        try (FileOutputStream outStream = new FileOutputStream(propertiesPath.toFile())) {
+            properties.store(outStream, null);
+        }
 
         restartManager.restart();
 
-        // Redirect to /search
         return "redirect:/search";
     }
 }
